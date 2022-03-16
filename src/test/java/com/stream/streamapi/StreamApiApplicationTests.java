ca33eadceaf2cb570f6793503cd15c3eb4417a3b
Никита <oleinik.nikita96@gmail.com>
2022-03-16 03:07:44 +0300

    Initial Commit

---
+++ src/test/java/com/stream/streamapi/StreamApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stream.streamapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamApiApplicationTests {
+
+    @Test
+    void contextLoads () {
+    }
+
+}
